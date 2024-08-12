4e58898b5ddf69ab2f99bf9f2d88902f3a3ab038
𓆩𝄟≛⃝ 𝐜𝐮𝐭𝐞𓆩Devil𓆪🦋≛⃝king𝄟𓆪 <166118712+CuteDevilKing@users.noreply.github.com>
2024-08-12 13:03:33 +0530

    Update play.py

--- DAXXMUSIC/plugins/play/play.py
+++ DAXXMUSIC/plugins/play/play.py
@@ -491,7 +491,7 @@ async def play_music(client, CallbackQuery, _):
     return await mystic.delete()
 
 
-@app.on_callback_query(filters.regex("DAXXmousAdmin") & ~BANNED_USERS)
+@app.on_callback_query(filters.regex("DEVILmousAdmin") & ~BANNED_USERS)
 async def DAXXmous_check(client, CallbackQuery):
     try:
         await CallbackQuery.answer(
@@ -502,7 +502,7 @@ async def DAXXmous_check(client, CallbackQuery):
         pass
 
 
-@app.on_callback_query(filters.regex("DAXXPlaylists") & ~BANNED_USERS)
+@app.on_callback_query(filters.regex("DEVILPlaylists") & ~BANNED_USERS)
 @languageCB
 async def play_playlists_command(client, CallbackQuery, _):
     callback_data = CallbackQuery.data.strip()
